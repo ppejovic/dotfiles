fe978ac1490a92a5b96fbccad68c2cd82f1e0035
Pero P <ppejovic@users.noreply.github.com>
2020-08-04 12:44:20 -0700

    Terraform install on ubuntu

--- packages.sh
+++ packages.sh
@@ -37,6 +37,7 @@ elif is_linux; then
         unzip
         tree
         screenfetch
+        wget
     )
 
     sudo apt install "${apps[@]}" -y
@@ -45,11 +46,19 @@ elif is_linux; then
     curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
     az extension add --name azure-devops
 
-    # AWS cli
     pushd /tmp/
+
+    # AWS cli
     curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
     unzip -o awscliv2.zip
-    sudo ./aws/install
+    sudo ./aws/install --update
+
+    # Terraform
+    TERR_VER=`curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest | grep tag_name | cut -d: -f2 | tr -d \"\,\v | awk '{$1=$1};1'`
+    wget https://releases.hashicorp.com/terraform/${TERR_VER}/terraform_${TERR_VER}_linux_amd64.zip
+    unzip terraform_${TERR_VER}_linux_amd64.zip
+    sudo mv terraform /usr/local/bin
+    
     popd
 
 fi
